d1b927b2e3946cb65a994f490d621d99fda8dc16
urvashi29 <urvashigolu29@gmail.com>
2022-08-28 21:41:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';//importing React, { Component } from 'react'(coming from node-modules)
+
+//App is a class component
+class App extends Component {
+
+    //method
+    render() {
+        return (
+            //jsx
+            <div>
+                <div>
+                    Hello
+                </div>
+                <div>
+                    World!
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default App;//exporting App component
+
+
+
+//task
+//understand folder structure
+//display some data and try running React Application
+
+
+
+
+
+
+
+//task
+//create an array of object and display data
+//create a object and update data(property) in it and display data
+//diffing algo
+//recoincilation process
+
+
+
